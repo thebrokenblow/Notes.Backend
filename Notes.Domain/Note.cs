5552284e6132065d90ef62d13381d51bd107e27b
Artem <Artem@DESKTOP-4BOPE1O>
2025-01-09 13:27:40 +0300

    Add project files.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Domain;
+
+public class Note
+{
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+}
