617b559f0f3d7de697ddfafd241301e066c5d8ea
Artem <Artem@DESKTOP-4BOPE1O>
2025-01-09 13:52:07 +0300

    Настроил работу с базой данных

---
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfigurations;
+
+public class NoteConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder
+            .HasKey(note => note.Id);
+
+        builder
+            .HasIndex(note => note.Id)
+            .IsUnique();
+
+        builder
+            .Property(note => note.Id)
+            .HasColumnName("id");
+
+        builder
+            .Property(note => note.Title)
+            .IsRequired()
+            .HasMaxLength(250)
+            .HasColumnName("title");
+
+        builder
+            .Property(note => note.Description)
+            .HasMaxLength(1000)
+            .HasColumnName("description");
+
+        builder
+            .Property(note => note.CreationDate)
+            .IsRequired()
+            .HasColumnName("creation_date");
+
+        builder
+            .Property(note => note.EditDate)
+            .IsRequired(false)
+            .HasColumnName("edit_date");
+
+        builder
+            .Property(note => note.UserId)
+            .IsRequired()
+            .HasColumnName("user_id");
+    }
+}
---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Persistence.Extensions;
+
+namespace Notes.Persistence;
+
+public class NotesDbContext(DbContextOptions<NotesDbContext> options) : DbContext(options)
+{
+    public required DbSet<Note> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyAllConfigurations();
+        base.OnModelCreating(modelBuilder);
+    }
+}
